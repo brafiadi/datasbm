937999458cc72fc7855a0c253e81be8de6ab3134
Ahdiat Brafiadi <brafiadi@ASUS-TUF-GAMING.>
2024-12-04 05:33:13 +0700

    minor update

--- src/services/master-data.service.ts
+++ src/services/master-data.service.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { getProvinsi } from "@prisma/client/sql";
 
 const prisma = new PrismaClient();
 
 const getAllProvinsi = async () => {
-	return await prisma.provinsi.findMany();
+	// return await prisma.provinsi.findMany();
+	// return await prisma.$queryRaw`
+	// 	SELECT *
+	// 	FROM provinsi
+	// `;
+	return await prisma.$queryRawTyped(getProvinsi());
 };
 
 export default {
